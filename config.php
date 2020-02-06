e9a6e296ce4170c71acd7f250f874992169c9455
Huib van Nieuwenhuijze <Huib.van.Nieuwenhuijze@gmail.com>
2020-02-06 20:39:22 +0100

    Added openhab config

--- config.php
+++ config.php
@@ -1,8 +1,7 @@
 <?php
-define('VERBOSE', 0);        // 0: be silent, except for errors; 1: be verbose
-define('IDCOUNT', N);
-define('APIKEY', 'hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh');
-define('SYSTEMID', 'NNNNNN');
+define('IDCOUNT', 4);
+define('APIKEY', 'a2dfaff1fda45ac9ecd17f60737953cfdf5d26f1');
+define('SYSTEMID', '36997');
 
 define('LIFETIME', 1);       // see README.md
 define('MODE', 'AGGREGATE'); // 'AGGREGATE' or 'SPLIT'
@@ -12,11 +11,12 @@ define('AC', 0);             // Send DC data or AC (DC * Efficiency)
 
 // If mode is SPLIT, define the Enecsys ID to PVOutput SystemID mapping for each
 // inverter.
-//$systemid = array(
-//  NNNNNNNNN => NNNNNN,
-//  NNNNNNNNN => NNNNNN,
-//  ...
-//);
+$systemid = array(
+  110106912 => 36997,
+  110103981 => 36997,
+  110103907 => 36997,
+  110133184 => 36997
+);
 
 // If mode is SPLIT, optionally define the Enecsys ID to APIKEY mappings
 // If an id is not found, the default APIKEY from above is used.
@@ -33,9 +33,14 @@ $ignored = array(
 
 
 // Optional MySQL defs, uncomment to enable MySQL inserts, see README.md
-//define('MYSQLHOST', 'localhost');
-//define('MYSQLUSER', 'myuser');
-//define('MYSQLPASSWORD', 'mypw');
-//define('MYSQLDB', 'mydbname');
-//define('MYSQLPORT', '3306');
+define('MYSQLHOST', '192.168.143.111');
+define('MYSQLUSER', 'datalogger');
+define('MYSQLPASSWORD', 'Z0iKswDUoXZo9QqYoI0B');
+define('MYSQLDB', 'HomeData');
+define('MYSQLPORT', '3307');
+
+// Optional OpenHab defs, uncomment to enable OpenHab updates
+define('OPENHABHOST', '192.168.143.128');
+define('OPENHABITEM', 'enecsys_raw_input');
+define('OPENHABPORT', '8088');
 ?>
